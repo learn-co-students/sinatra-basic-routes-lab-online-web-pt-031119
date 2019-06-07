a4dc2c77cf119cf6c2726be18f9b1e3fd23bf95e
Nicholas Rodriguez <niko876@hotmail.com>
2019-06-07 13:51:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Nicholas Rodriguez."
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn, NY."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I have no clue!"
+  end
+
 end
