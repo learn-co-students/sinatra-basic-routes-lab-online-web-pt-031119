45a01ebdd1c7fe2d455de1554687dc3d6c714c02
Mark Kanko <mkanko66@gmail.com>
2019-06-08 14:51:12 -0700

    completed sinatra-basic-routes-lab

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class AppController < Sinatra::Base
+    get '/name' do
+        "My name is Dude." 
+    end
+    
+    get '/hometown' do 
+        "My hometown is all Cali Cool!"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is no one favorite."
+    end 
+
 end
