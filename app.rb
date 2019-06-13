86784d58a197b847961aee485b62cf8fd05e57b6
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-13 18:39:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Dani"
+  end
+  
+  get '/hometown' do
+    "My hometown is Madrid"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Come out and see"
+  end
+  
 end
